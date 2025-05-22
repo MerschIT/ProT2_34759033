15f915c029cfe62b9ea69282263acefe9ef7afca
augusto <augusto@prueba.com.ar>
2025-05-21 22:29:32 -0300

    Se agregó un banner y se añadió la palabra mas al lado del boton hamburguesa

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -2,13 +2,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HEAD_VIEW</title>
+    <title>PACU FAST FOOD - CORRIENTES</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/miestilo.css" rel="stylesheet">
 </head>
 <header>
-    <h1>Primer Proyecto HEAD_VIEW</h1>    
+    <img src="assets/img/banner.png" alt="Banner"  class="img-banner" id="banner">   
 </header>
 <body>
     
--- app/Views/front/navbar_view.php
+++ app/Views/front/navbar_view.php
@@ -1,6 +1,6 @@
 <!-- INICIO NAVBAR -->
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg mi-navbar-bg">
   <div class="container-fluid">
 <!-- AGREGO MI CODIGO DE IMAGEN DE LOGO -->
     <a class="navbar-brand" href="#">
@@ -10,9 +10,11 @@
 <!-- FIN IMAGEN DE LOGO -->
     
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <a class="navbar-brand" href="#">Más</a><span class="navbar-toggler-icon"></span>
     </button>
+    
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
