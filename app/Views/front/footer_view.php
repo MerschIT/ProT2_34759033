7699f9ce762a2a14c8faa938a1e9381f0fbe826d
augusto <augusto@prueba.com.ar>
2025-05-25 23:35:31 -0300

    Se hicieron modificaciones al footer en cuanto a los logos

--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -1,10 +1,10 @@
  <footer>
             <P>SOY UN FOOTER</P>
-            <nav class="logo-contenedor">
-            <UL>
-                <li class="logo-contenedor"><a href="https://google.com" class="link"><img src="assets/img/google_logo.png" alt="Logo Google" class="logo">Google</a></li>
-                <li class="logo-contenedor"><a href="https://yahoo.com" class="link"><img src="assets/img/yahoo_logo.svg" alt="Logo Yahoo" class="logo">Yahoo</a></li>
-                <li class="logo-contenedor"><a href="https://talentosdigitales.ar/" class="link"><img src="assets/img/talentos_d_logo.png" alt="Logo Talentos Digitales" class="logo">Talentos Digitales</a></li>
+            <nav class="logo-contenedor w-100">
+            <ul class="d-flex justify-content-around align-items-center list-unstyled m-0 p-0 w-100">
+                <li class="logo-contenedor logo-footer"><a href="https://google.com" class="link"><img src="assets/img/google_logo.png" alt="Logo Google" class="logo-footer"><span class="ms-2">Google</span></a></li>
+                <li class="logo-contenedor logo-footer"><a href="https://yahoo.com" class="link"><img src="assets/img/yahoo_logo.svg" alt="Logo Yahoo" class="logo-footer"><span class="ms-2">Yahoo!</span></a></li>
+                <li class="logo-contenedor logo-footer"><a href="https://talentosdigitales.ar/" class="link"><img src="assets/img/talentos_d_logo.png" alt="Logo Talentos Digitales" class="logo-footer"><span>Talentos Digitales</span></a></li>
 
             </UL>
         </nav>
