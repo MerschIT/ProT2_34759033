2d9865910cfe497e0a9b27639441a7e7c1f1d0eb
augusto <augusto@prueba.com.ar>
2025-05-25 21:26:48 -0300

    Se agregan RUTAS. Se crea pagina de login

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,13 +4,23 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
         return
          view('front/head_view').
          view('front/navbar_view').
          view('front/principal').
          view('front/cards_view').
+         view('front/footer_view');
+    }
+
+     public function login()
+    {
+        return
+         view('front/head_view').
+         view('front/navbar_view').
+         view('front/login').
+         
          view('front/footer_view');
     }
 }
