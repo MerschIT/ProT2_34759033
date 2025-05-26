82ac47214a38472eeeb7a763cc5f38c56ce16190
augusto <augusto@prueba.com.ar>
2025-05-26 20:06:37 -0300

    Se mejora altura y tamaño de los logos en el footer

--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -1,10 +1,10 @@
  <footer>
             <P>SOY UN FOOTER</P>
             <nav class="logo-contenedor w-100">
-            <ul class="d-flex justify-content-around align-items-center list-unstyled m-0 p-0 w-100">
+            <ul class="d-flex justify-content-center align-items-center list-unstyled m-0 p-0 w-100 pb-3">
                 <li class="logo-contenedor logo-footer"><a href="https://google.com" class="link"><img src="assets/img/google_logo.png" alt="Logo Google" class="logo-footer"><span class="ms-2">Google</span></a></li>
                 <li class="logo-contenedor logo-footer"><a href="https://yahoo.com" class="link"><img src="assets/img/yahoo_logo.svg" alt="Logo Yahoo" class="logo-footer"><span class="ms-2">Yahoo!</span></a></li>
-                <li class="logo-contenedor logo-footer"><a href="https://talentosdigitales.ar/" class="link"><img src="assets/img/talentos_d_logo.png" alt="Logo Talentos Digitales" class="logo-footer"><span>Talentos Digitales</span></a></li>
+                <li class="logo-contenedor logo-footer"><a href="https://talentosdigitales.ar/" class="link"><img src="assets/img/talentos_d_logo.png" alt="Logo Talentos Digitales" class="logo-footer"></a></li>
 
             </UL>
         </nav>
