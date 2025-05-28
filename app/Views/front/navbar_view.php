d04175e5091ed8778e4baa6818634af89df75c34
augusto <augusto@prueba.com.ar>
2025-05-27 22:24:37 -0300

    Se corrige detalle en texto del cuadro de busqueda en el NAV

--- app/Views/front/navbar_view.php
+++ app/Views/front/navbar_view.php
@@ -59,8 +59,8 @@
         
       </ul>
       <form class="d-flex pt-2" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <input class="form-control me-2" type="search" placeholder="Que quieres comer" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
       </form>
     </div>
   </div>
