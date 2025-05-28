d78e67298e146474a0514bec181550e4c0532f38
augusto <augusto@prueba.com.ar>
2025-05-27 22:00:27 -0300

    Se corrige la baseURL del proyecto, se quita la IP estática y se vuelve a colocar LOCALHOST para que pueda ser clonado desde Github y no genere errores al levantar la web

--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -1,6 +1,6 @@
  <footer>
-            <P>SOY UN FOOTER</P>
-            <nav class="logo-contenedor w-100">
+            
+            <nav class="logo-contenedor w-100 mb-0">
             <ul class="d-flex justify-content-center align-items-center list-unstyled m-0 p-0 w-100 pb-3">
                 <li class="logo-contenedor logo-footer"><a href="https://google.com" class="link"><img src="assets/img/google_logo.png" alt="Logo Google" class="logo-footer"><span class="ms-2">Google</span></a></li>
                 <li class="logo-contenedor logo-footer"><a href="https://yahoo.com" class="link"><img src="assets/img/yahoo_logo.svg" alt="Logo Yahoo" class="logo-footer"><span class="ms-2">Yahoo!</span></a></li>
